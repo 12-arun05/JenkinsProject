697937b9d9c84c3fa8d1ae907a1ffccf0376c8eb
Gandhimathinathan S <140043590+12-arun05@users.noreply.github.com>
2025-06-30 21:38:37 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,8 @@
+public class HelloGuys{
+  public static void main(String[] args){
+    System.out.println("Hello, Arun");
+    System.out.println("Hello, Dharani");
+    System.out.println("Hello, Gandhi");
+    System.out.println("Hello, Devi");
+  }
+}
